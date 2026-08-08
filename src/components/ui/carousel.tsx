68fe08212de155081137362a643adc168b4944dd
hilfikiri1 <kyrylopodolskyi@gmail.com>
2026-08-08 03:30:32 +0200

    Avoid synchronous carousel state updates in effects

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -95,12 +95,15 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
+
     api.on("reInit", onSelect)
     api.on("select", onSelect)
+    const frameId = window.requestAnimationFrame(() => onSelect(api))
 
     return () => {
-      api?.off("select", onSelect)
+      window.cancelAnimationFrame(frameId)
+      api.off("reInit", onSelect)
+      api.off("select", onSelect)
     }
   }, [api, onSelect])
 
